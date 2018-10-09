6e5796cea447149683aa94bc62cc8453b0771e90
unknown <illya199614@gmail.com>
2018-10-09 11:21:26 +0300

    Issue with character

---
+++ Assets/Scripts/CarWheel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarWheel : MonoBehaviour {
+
+    public WheelCollider targetWheel;
+    private Vector3 wheelPositiom = new Vector3();
+    private Quaternion wheelRotation = new Quaternion();
+	
+	void Update ()
+    {
+        targetWheel.GetWorldPose(out wheelPositiom,out wheelRotation);
+        transform.position = wheelPositiom;
+        transform.rotation = wheelRotation;
+	}
+}
